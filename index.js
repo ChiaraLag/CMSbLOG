b392464fad02b6a206fa89a426e2ea1cfa8dece3
Chiara <chiaralagana.219@gmail.com>
2020-10-30 14:10:27 +0100

    comt 4

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express = require('express'),
 app.use(express.static(__dirname = '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname = "/index.html"));
+    res.sendFile(path.join(__dirname = '/index.html'));
 })
 
 app.listen(port);
