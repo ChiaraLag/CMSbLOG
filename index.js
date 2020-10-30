31e5dbdfa3a7c19e17202ec5a13780cfea16c859
Chiara <chiaralagana.219@gmail.com>
2020-10-30 12:56:11 +0100

    cmt2

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000;
     path = require('path')
-    
+
 app.use(express.static(__dirname = '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname = "/index.html"));
+    res.sendFile(path.join(__dirname = "./index.html"));
 })
 
 app.listen(port);
